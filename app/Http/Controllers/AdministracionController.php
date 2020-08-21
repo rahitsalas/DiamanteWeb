96b7cb96e8fb2ec39af92b5fcfba85764aab8313
Rahit <rahit.salas@gmail.com>
2020-08-21 13:23:22 -0500

    'agregados_graficos_compras_segunda_parte'

--- app/Http/Controllers/AdministracionController.php
+++ app/Http/Controllers/AdministracionController.php
@@ -40,10 +40,12 @@ class AdministracionController extends Controller
         $raw1 = DB::select("exec [DiamanteWeb].dbo.sp_data_OrdenCompraServicioSoles ");
         $raw2 = DB::select("exec [DiamanteWeb].dbo.sp_data_OrdenCompraServicioGastos ");
         $raw3 = DB::select("exec [DiamanteWeb].dbo.sp_data_OrdenCompraServicioActivos ");
+        $raw4 = DB::select("exec [DiamanteWeb].dbo.sp_data_OrdenCompraServicioCantidad ");
+        $raw5 = DB::select("exec [DiamanteWeb].dbo.sp_data_OrdenCompraServicioMix ");
        // dd($raw1);
 
-        $raw5 = DB::select("exec [DiamanteWeb].dbo.sp_data_DespachoDiarioMillar '".$firstDay."', '".$lastDay."'");
-        $raw6 = DB::select("exec [DiamanteWeb].dbo.sp_data_DespachoDiarioSoles '".$firstDay."', '".$lastDay."'");
+       // $raw5 = DB::select("exec [DiamanteWeb].dbo.sp_data_DespachoDiarioMillar '".$firstDay."', '".$lastDay."'");
+      //  $raw6 = DB::select("exec [DiamanteWeb].dbo.sp_data_DespachoDiarioSoles '".$firstDay."', '".$lastDay."'");
 //        dd($startDate,$raw5,$raw6);
 //        $firstDay = Carbon::now()->addDays(-1)->startOfMonth();
 //        $lastDay = Carbon::now()->addDays(-1)->lastOfMonth();
@@ -56,6 +58,15 @@ class AdministracionController extends Controller
 
         $dataOrdenCompraServicioActivosActual  = array();
         $dataOrdenCompraServicioActivosPasado = array();
+
+        $dataOrdenCompraServicioCantidadActual  = array();
+        $dataOrdenCompraServicioCantidadPasado = array();
+
+        $dataOrdenCompraServicioMixActualOS = array();
+        $dataOrdenCompraServicioMixActualOC = array();
+        $dataOrdenCompraServicioMixPasadoOS  = array();
+        $dataOrdenCompraServicioMixPasadoOC = array();
+
         $dataDespachoDiaroMillar = array();
         $dataDespachoDiaroSoles = array();
 
@@ -126,6 +137,67 @@ class AdministracionController extends Controller
             }
         }
 
+        $j =0;
+        $k =0;
+        foreach ($raw4 as $item){
+//            var_dump($item);
+            if((string)$item->año === (string)$startDate->year ) {
+//                var_dump("-----------------");
+//                var_dump((string)$startDate->year);
+                //var_dump((string)$item->año);
+                //$dataDespachoDiaroMillar['fecha'][$i] = date_create($item->fecha)->format('d-m');
+                $dataOrdenCompraServicioCantidadActual['cantidad'][$j] = $item->cantidad;
+                $dataOrdenCompraServicioCantidadActual['año'] = $item->año;
+                $dataOrdenCompraServicioCantidadActual['mes'][$j] = $item->mesdescripcion;
+                $j++;
+            }
+            elseif((string)$item->año === (string)($startDate->year-1)){
+                $dataOrdenCompraServicioCantidadPasado['cantidad'][$k] = $item->cantidad;
+                $dataOrdenCompraServicioCantidadPasado['año'] = $item->año;
+                $dataOrdenCompraServicioCantidadPasado['mes'][$k] = $item->mesdescripcion;
+                $k++;
+            }
+        }
+
+        $j =0;
+        $k =0;
+        $l =0;
+        $i =0;
+        //dd($raw5);
+        foreach ($raw5 as $item){
+//            var_dump($item);
+            if((string)$item->año === (string)$startDate->year and $item->clasificacion == 'Orden de Servicio') {
+//                var_dump("-----------------");
+//                var_dump((string)$startDate->year);
+                //var_dump((string)$item->año);
+                //$dataDespachoDiaroMillar['fecha'][$i] = date_create($item->fecha)->format('d-m');
+                $dataOrdenCompraServicioMixActualOS['porcentaje'][$j] = round((double)$item->porcentaje, 0, PHP_ROUND_HALF_UP);
+                $dataOrdenCompraServicioMixActualOS['clasificacion'] = $item->clasificacion;
+                $dataOrdenCompraServicioMixActualOS['mes'][$j] = (string)($startDate->year-1).' '.$item->mesdescripcion.' '.(string)($startDate->year);
+                $j++;
+            }
+            elseif((string)$item->año === (string)($startDate->year-1) and $item->clasificacion == 'Orden de Servicio'){
+                $dataOrdenCompraServicioMixPasadoOS['porcentaje'][$k] = round((double)$item->porcentaje, 0, PHP_ROUND_HALF_UP);
+                $dataOrdenCompraServicioMixPasadoOS['clasificacion'] = $item->clasificacion;
+                $dataOrdenCompraServicioMixPasadoOS['mes'][$k] = $item->mesdescripcion;
+                $k++;
+            }
+            elseif((string)$item->año === (string)$startDate->year and $item->clasificacion == 'Orden de Compra'){
+                $dataOrdenCompraServicioMixActualOC['porcentaje'][$l] = round((double)$item->porcentaje, 0, PHP_ROUND_HALF_UP);
+                $dataOrdenCompraServicioMixActualOC['clasificacion'] = $item->clasificacion;
+                $dataOrdenCompraServicioMixActualOC['mes'][$l] = $item->mesdescripcion;
+                $l++;
+            }
+            elseif((string)$item->año === (string)($startDate->year-1) and $item->clasificacion == 'Orden de Compra'){
+                $dataOrdenCompraServicioMixPasadoOC['porcentaje'][$i] = round((double)$item->porcentaje, 0, PHP_ROUND_HALF_UP);
+                $dataOrdenCompraServicioMixPasadoOC['clasificacion'] = $item->clasificacion;
+                $dataOrdenCompraServicioMixPasadoOC['mes'][$i] = $item->mesdescripcion;
+                $i++;
+            }
+        }
+
+        //dd($dataOrdenCompraServicioMixActualOS,$dataOrdenCompraServicioMixPasadoOS,$dataOrdenCompraServicioMixActualOC,$dataOrdenCompraServicioMixPasadoOC);
+
         //dd($dataOrdenCompraServicioSolesActual,$dataOrdenCompraServicioSolesPasado);
 
 //        $i =0;
@@ -152,7 +224,11 @@ class AdministracionController extends Controller
         return view ('Dashboard.Reportes.administracion',compact('startDate',
             'dataOrdenCompraServicioSolesActual','dataOrdenCompraServicioSolesPasado',
             'dataOrdenCompraServicioGastosActual', 'dataOrdenCompraServicioGastosPasado',
-            'dataOrdenCompraServicioActivosActual', 'dataOrdenCompraServicioActivosPasado'));
+            'dataOrdenCompraServicioActivosActual', 'dataOrdenCompraServicioActivosPasado',
+            'dataOrdenCompraServicioCantidadActual','dataOrdenCompraServicioCantidadPasado',
+            'dataOrdenCompraServicioMixActualOS','dataOrdenCompraServicioMixPasadoOS',
+            'dataOrdenCompraServicioMixActualOC','dataOrdenCompraServicioMixPasadoOC'
+        ));
     }
 }
 
--- resources/views/Dashboard/Reportes/administracion.blade.php
+++ resources/views/Dashboard/Reportes/administracion.blade.php
@@ -192,6 +192,67 @@
                 {{--                                </div>--}}
                 {{--                            @endif--}}
             </div>
+
+            <div class="row">
+                {{--                @if(Auth::user()->id==1)--}}
+                {{--                    <div class="col-md-9">--}}
+                {{--                        @else--}}
+                <div class="col-md-12">
+                    {{--                                @endif--}}
+                    <div class="card ">
+                        <div class="card-header border-0">
+                            <div class="d-flex justify-content-between">
+                                <h3 class="card-title">Cantidad de Órdenes de Compra y Servicio</h3>
+                                {{--                                <a href="javascript:void(0);">View Report</a>--}}
+                            </div>
+                        </div>
+                        <div class="card-body pt-0 mt-0">
+                            <div class="d-flex">
+                                <p class="d-flex flex-column">
+                                    {{--                                                <span class="text-bold text-lg">Total {{$dataDespachoDiaroSoles['total']}} Miles de Soles</span>--}}
+                                    <span></span>
+                                </p>
+                            </div>
+                            <!-- /.d-flex -->
+
+                            <div class="position-relative mb-4">
+                                <canvas id="ordencompraserviciocantidad-chart" height="200"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                {{--                @if(Auth::user()->id==1)--}}
+                {{--                    <div class="col-md-9">--}}
+                {{--                        @else--}}
+                <div class="col-md-12">
+                    {{--                                @endif--}}
+                    <div class="card ">
+                        <div class="card-header border-0">
+                            <div class="d-flex justify-content-between">
+                                <h3 class="card-title">Comparativo Mix O. Compra vs O. Servicio (%)</h3>
+                                {{--                                <a href="javascript:void(0);">View Report</a>--}}
+                            </div>
+                        </div>
+                        <div class="card-body pt-0 mt-0">
+                            <div class="d-flex">
+                                <p class="d-flex flex-column">
+                                    {{--                                                <span class="text-bold text-lg">Total {{$dataDespachoDiaroSoles['total']}} Miles de Soles</span>--}}
+                                    <span></span>
+                                </p>
+                            </div>
+                            <!-- /.d-flex -->
+
+                            <div class="position-relative mb-4">
+                                <canvas id="ordencompraserviciomix-chart" height="200"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
         <!-- /.container-fluid -->
     </div>
@@ -318,10 +379,12 @@
                 options: {
                     scales: {
                         xAxes: [{
-                            display: true
+                            display: true,
+                            // stacked: true
                         }],
                         yAxes: [{
                             display: true,
+                            // stacked: true
                         }]
                     },
                     responsive: true,
@@ -345,14 +408,14 @@
                         borderWidth: 1
                     },
                         {
-                            label: {!! json_encode($dataOrdenCompraServicioActivosActual['año']) !!},
-                            data:{!! json_encode($dataOrdenCompraServicioActivosActual['montototal']) !!},
-                            backgroundColor: //[
-                                'rgba(115,255,64,0.2)',
+                        label: {!! json_encode($dataOrdenCompraServicioActivosActual['año']) !!},
+                        data:{!! json_encode($dataOrdenCompraServicioActivosActual['montototal']) !!},
+                        backgroundColor: //[
+                            'rgba(115,255,64,0.2)',
 
-                            borderColor: //[
-                                'rgb(42, 177, 66, 1)',
-                            borderWidth: 1
+                        borderColor: //[
+                            'rgb(42, 177, 66, 1)',
+                        borderWidth: 1
                         }
                     ]
                 },
@@ -367,6 +430,168 @@
                     },
                     responsive: true,
                     maintainAspectRatio: false,
+                }
+            });
+
+            var ctx = document.getElementById('ordencompraserviciocantidad-chart').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: {!! json_encode($dataOrdenCompraServicioCantidadPasado['mes']) !!},
+                    datasets: [{
+                        label: {!! json_encode($dataOrdenCompraServicioCantidadPasado['año']) !!},
+                        data:{!! json_encode($dataOrdenCompraServicioCantidadPasado['cantidad']) !!},
+                        backgroundColor: //[
+                            'rgba(54, 162, 235, 0.2)',
+
+                        borderColor: //[
+                            'rgba(54, 162, 235, 1)',
+                        borderWidth: 1
+                    },
+                        {
+                            label: {!! json_encode($dataOrdenCompraServicioCantidadActual['año']) !!},
+                            data:{!! json_encode($dataOrdenCompraServicioCantidadActual['cantidad']) !!},
+                            backgroundColor: //[
+                                'rgba(255, 159, 64, 0.2)',
+
+                            borderColor: //[
+                                'rgba(255, 159, 64, 1)',
+                            borderWidth: 1
+                        }
+                    ]
+                },
+                options: {
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            // stacked: true
+                        }],
+                        yAxes: [{
+                            display: true,
+                            // stacked: true
+                        }]
+                    },
+                    responsive: true,
+                    maintainAspectRatio: false,
+                }
+            });
+
+            var ctx = document.getElementById('ordencompraserviciomix-chart').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: {!! json_encode($dataOrdenCompraServicioMixActualOS['mes']) !!},
+                    // labels: [[["2019", "2020"], "Active: 2 hrs"],
+                    //     [["2019", "2020"], "Active: 2 hrs"],
+                    //     [["2019", "2020"], "Active: 2 hrs"],
+                    //     [["2019", "2020"], "Active: 2 hrs"]],
+            //
+            //         [["2019", "2020"], "Enero"],
+            // [["2019", "2020"], "Febrero"],
+            //     [["2019", "2020"], "Marzo"],
+            //     [["2019", "2020"], "Abril"],
+                // [["2019", "2020"], "Mayo"],
+                // [["2019", "2020"], "Junio"],
+                // [["2019", "2020"], "Julio"],
+                // [["2019", "2020"], "Agosto"],
+                // [["2019", "2020"], "Septiembre"],
+                // [["2019", "2020"], "Octubre"],
+                // [["2019", "2020"], "Noviembre"],
+                // [["2019", "2020"], "Diciembre"],
+
+                datasets: [
+
+                        {
+                            label: {!! json_encode($dataOrdenCompraServicioMixPasadoOC['clasificacion']) !!},
+                            data:{!! json_encode($dataOrdenCompraServicioMixPasadoOC['porcentaje']) !!},
+                            backgroundColor: //[
+                                'rgba(54, 162, 235, 0.2)',
+
+                            borderColor: //[
+                                'rgba(54, 162, 235, 1)',
+                            borderWidth: 1,
+                            stack: 'OrdenCompra'
+                        },
+
+                        {
+                            label: {!! json_encode($dataOrdenCompraServicioMixActualOC['clasificacion']) !!},
+                            data:{!! json_encode($dataOrdenCompraServicioMixActualOC['porcentaje']) !!},
+
+                            backgroundColor: //[
+                                'rgba(54, 162, 235, 0.2)',
+
+                            borderColor: //[
+                                'rgba(54, 162, 235, 1)',
+                            borderWidth: 1,
+                            stack: 'OrdenServicio'
+                        },
+
+                        {
+                            label: {!! json_encode($dataOrdenCompraServicioMixPasadoOS['clasificacion']) !!},
+                            data:{!! json_encode($dataOrdenCompraServicioMixPasadoOS['porcentaje']) !!},
+                            backgroundColor: //[
+                                'rgba(255, 99, 132, 0.2)',
+
+                            borderColor: //[
+                                'rgba(255, 99, 132, 1)',
+                            borderWidth: 1,
+                            stack: 'OrdenCompra'
+                        },
+
+                        {
+                            label: {!! json_encode($dataOrdenCompraServicioMixActualOS['clasificacion']) !!},
+                            label: 'OC',
+                            data:{!! json_encode($dataOrdenCompraServicioMixActualOS['porcentaje']) !!},
+                            backgroundColor: //[
+                                'rgba(255, 99, 132, 0.2)',
+
+                            borderColor: //[
+                                'rgba(255, 99, 132, 1)',
+                            borderWidth: 1,
+                            stack: 'OrdenServicio',
+                            // hiddenLegend: true,
+                        },
+
+
+                    ]
+                },
+                options: {
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            // stacked: true
+                        }],
+                        yAxes: [{
+                            display: true,
+                            // stacked: true
+                            ticks: {
+                                // Include a dollar sign in the ticks
+                                callback: function(value, index, values) {
+                                    return  value+'%';
+                                }
+                            }
+                        }]
+                    },
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    legend: {
+                        labels: {
+                            filter: function (legendItem, chartData) {
+                                if (legendItem.datasetIndex === 1 || legendItem.datasetIndex === 3) {
+                                    return false;
+                                }
+                                return true;
+                            }
+                        }
+                    }
+                    // legend: {
+                    //     labels: {
+                    //         filter: function(item, chart) {
+                    //             // Logic to remove a particular legend item goes here
+                    //             return !item.text.includes('OC');
+                    //         }
+                    //     }
+                    // }
                 }
             });
 
