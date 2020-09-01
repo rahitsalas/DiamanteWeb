14ab1645d91529805b63a478c096028d722b999a
Rahit <rahit.salas@gmail.com>
2020-09-01 09:16:46 -0500

    'cambios_minimos_bd_colores0

--- config/database.php
+++ config/database.php
@@ -85,7 +85,7 @@ return [
             'port' => env('DB_PORT', '1433'),
             'database' => env('DB_DATABASE', 'DiamanteWEB'),
             'username' => env('DB_USERNAME', 'sa'),
-            'password' => env('DB_PASSWORD', 'Server.2015'),
+            'password' => env('DB_PASSWORD', 'Studio@2014'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
